1eb824dffb59007cd382180e7eabb7d994af5c4f
Leevi Limnell <leevi.limnell@gmail.com>
2020-11-22 21:10:04 +0200

    test

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 'use strict';
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const rootRoute = require('./routes/rootRoute');
 const catRoute = require('./routes/catRoute');
 const userRoute = require('./routes/userRoute');
+
 const app = express();
-const port = 3000;
 
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json())
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV === 'production') {
+  require('./production')(app, process.env.PORT);
+} else {
+  require('./localhost')(app, process.env.HTTPS_PORT, process.env.HTTP_PORT);
+}
+
+// routes
 app.use('/', rootRoute);
-app.use('/cat', catRoute);
+app.use('/cat',  catRoute);
 app.use('/user', userRoute);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
